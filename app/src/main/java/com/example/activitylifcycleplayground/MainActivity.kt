5ab94b393e82f17b13f7def05b166d94d75fccbf
Alan Amalemba <alan.amalemba@lctafrica.net>
2025-08-05 09:07:16 +0300

    use onDestroy and save text in file during onDestroy

--- app/src/main/java/com/example/activitylifcycleplayground/MainActivity.kt
+++ app/src/main/java/com/example/activitylifcycleplayground/MainActivity.kt
@@ -1,26 +1,19 @@
 package com.example.activitylifcycleplayground
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.activitylifcycleplayground.databinding.ActivityMainBinding
-import java.util.Timer
-import kotlin.concurrent.fixedRateTimer
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var seconds = 0
-    private lateinit var timer: Timer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         binding.buttonExit.setOnClickListener {
@@ -28,20 +21,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onPause() {
-        super.onPause()
-        timer.cancel()
-    }
+    override fun onStop() {
+        super.onStop()
 
-    override fun onResume() {
-        super.onResume()
-        timer = fixedRateTimer(period = 1000L) {
-            runOnUiThread {
-                seconds++
-                binding.textViewRefreshStatus.text =
-                    "You have been staring at the screen for $seconds seconds"
+        val userMessage = binding.editTextMessage.text.toString()
 
-            }
-        }
+        File(filesDir, "user_message.txt").writeText(userMessage, Charsets.UTF_8)
     }
 }

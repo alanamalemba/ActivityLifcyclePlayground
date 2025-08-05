76f2ee96852ca23c98063eb810c1adddf4aad081
Alan Amalemba <alan.amalemba@lctafrica.net>
2025-08-05 09:56:14 +0300

    intercept the back button

--- app/src/main/java/com/example/activitylifcycleplayground/MainActivity.kt
+++ app/src/main/java/com/example/activitylifcycleplayground/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.activitylifcycleplayground
 
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.activitylifcycleplayground.databinding.ActivityMainBinding
 import java.io.File
@@ -9,23 +12,29 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val userMessage = binding.editTextMessage.text.toString()
+                File(filesDir, "user_message.txt").writeText(userMessage, Charsets.UTF_8)
+                finish()
+            }
+        }
+
+        onBackPressedDispatcher.addCallback(this, callback)
+
 
         binding.buttonExit.setOnClickListener {
             finish()
         }
     }
 
-    override fun onStop() {
-        super.onStop()
-
-        val userMessage = binding.editTextMessage.text.toString()
-
-        File(filesDir, "user_message.txt").writeText(userMessage, Charsets.UTF_8)
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
